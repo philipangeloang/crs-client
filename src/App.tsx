a1a9de29982ba40e3181269b01286f37ef6f1bbf
philipangeloang <philipangeloang1@gmail.com>
2023-10-21 02:20:38 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="grid place-items-center h-screen">
+        <Button>Click Me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
