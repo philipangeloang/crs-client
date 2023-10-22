5280c797d77883707b0f5394e234bd93a31f3cee
philipangeloang <philipangeloang1@gmail.com>
2023-10-23 01:12:37 +0800

    Fixed minor issue Signin Module

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
 import { Button } from "./components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 
 function App() {
   return (
