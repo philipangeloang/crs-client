ac19ff2324bbd2ad24ece3c72cf6917accb9f3ff
philipangeloang <philipangeloang1@gmail.com>
2023-10-23 01:31:50 +0800

    Editied initial responsiveness of Signin Module

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <>
       <div className="mx-auto max-w-[100rem] px-16 flex flex-col my-3">
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col justify-between items-center | md:flex-row">
           <img src="/plm_logo.png" className="h-20" />
-          <h1 className="font-montserrat font-bold text-xl text-[#434343]">
+          <h1 className="font-montserrat font-bold text-xl text-[#434343] mt-5 | md:mt-0">
             The PLM Online Campus
           </h1>
         </div>
@@ -15,7 +15,7 @@ function App() {
       <div>
         <img src="/divider_signin.png" className="w-full h-3" />
       </div>
-      <div className="mx-auto max-w-[100rem] px-16 flex flex-col py-32 justify-center items-center ">
+      <div className="mx-auto max-w-[100rem] py-16 px-16 flex flex-col justify-center items-center | md:py-32 ">
         <div className="py-14 px-16 border border-black rounded-md w-[28rem]  font-open-sans relative z-10 bg-white">
           <h1 className="font-bold font-montserrat text-2xl mb-2 text-main-red">
             Sign In
@@ -37,7 +37,7 @@ function App() {
         <div className="w-[28rem] flex justify-between">
           <Button
             variant="link"
-            className="text-xs font-normal text-main-blue underline underline-offset-0 font-open-sans"
+            className="text-xs relative z-10 font-normal text-main-blue underline underline-offset-0 font-open-sans"
           >
             Can't Access your account?
           </Button>
@@ -54,15 +54,15 @@ function App() {
         />
       </div>
       <div className="w-full bg-[#434343] absolute bottom-0">
-        <div className="max-w-[100rem] px-16 py-12 mx-auto flex flex-col justify-center  font-open-sans">
-          <div className="flex justify-between mb-1 text-sm">
-            <p className="text-white">
+        <div className="max-w-[100rem] px-16 py-12 mx-auto flex flex-col justify-center font-open-sans">
+          <div className="flex flex-col justify-between items-center mb-1 text-sm | md:flex-row md:items-start">
+            <p className="text-white text-center">
               This is the official CRS webiste of{" "}
               <strong>Pamantasan ng Lungsod ng Maynila</strong>
             </p>
             <p className="text-white">For inquiries and concerns:</p>
           </div>
-          <div className="text-sm flex justify-between">
+          <div className="text-sm flex flex-col justify-between items-center | md:flex-row md:items-start">
             <p className="text-main-yellow">
               © 2023 ONPLM. All rights reserved.
             </p>
