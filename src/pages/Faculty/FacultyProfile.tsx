0b06982ecf62642683176069d6d2838e3486108f
Roi John Belmonte <roibelmonte@gmail.com>
2023-10-30 16:38:29 +0800

    Assigned hardcoded faculty values to objects

--- src/pages/Faculty/FacultyProfile.tsx
+++ src/pages/Faculty/FacultyProfile.tsx
@@ -4,6 +4,35 @@ import { BsGlobe2 } from "react-icons/bs";
 import { BiUserPin } from "react-icons/bi";
 import DateTime from "@/components/DateTime";
 
+// Faculty Details
+let personalDetails = {
+  employeeNumber: "2019-81725",
+  lastName: "DELA CRUZ",
+  firstName: "JUAN",
+  middleName: "PEPITO",
+  nameExtension: "",
+  maidenName: "",
+  pedigree: "None",
+  birthDate: "May 3, 1987",
+  birthPlace: "NCR - Manila",
+  gender: "Male",
+  civilStatus: "Single",
+  citizenship: "Filipino",
+  mobileNumber: "09987654321",
+  emailAddress: "juan.delacruz@gmail.com"
+};
+
+let employmentDetails = {
+  tin: "xxx-xxx-xxx-xxx",
+  gsisNumber: "xxxx-xxxx-xxxx-xxxx",
+  instructorCode: "DELACRUZJP"
+};
+
+let currentAddress = {
+  address: "5317 Muralla St., Intramuros, Manila, Philippines 1002",
+  phoneNumber: "(02) 8643 2500",
+};
+
 const FacultyProfile = () => {
   return (
     <>
@@ -40,51 +69,51 @@ const FacultyProfile = () => {
               <div className="w-full flex flex-row flex-start">
                 <div className="w-1/3 flex flex-row flex-start">
                   <span className="w-1/3 text-left">Employee No. :</span>
-                  <span className="w-2/3 text-left font-bold">2019-81725</span>
+                  <span className="w-2/3 text-left font-bold">{personalDetails.employeeNumber}</span>
                 </div>
               </div>
               <div className="w-full flex flex-row flex-start">
                 <div className="w-1/3 flex flex-row flex-start">
                   <span className="w-1/3 text-left">Full Name :</span>
-                  <span className="w-2/3 text-left font-bold">JUAN PEPITO DELA CRUZ</span>
+                  <span className="w-2/3 text-left font-bold">{personalDetails.firstName} {personalDetails.middleName} {personalDetails.lastName} {personalDetails.nameExtension}</span>
                 </div>
                 <div className="w-1/3 flex flex-row flex-start">
                   <span className="w-1/3 text-left">Pedigree :</span>
-                  <span className="w-2/3 text-left font-bold">None</span>
+                  <span className="w-2/3 text-left font-bold">{personalDetails.pedigree}</span>
                 </div>
               </div>
               <div className="w-full flex flex-row flex-start">
                 <div className="w-1/3 flex flex-row flex-start">
                   <span className="w-1/3 text-left">Birth Date :</span>
-                  <span className="w-2/3 text-left font-bold">May 3, 1987</span>
+                  <span className="w-2/3 text-left font-bold">{personalDetails.birthDate}</span>
                 </div>
                 <div className="w-1/3 flex flex-row flex-start">
                   <span className="w-1/3 text-left">Birth Place :</span>
-                  <span className="w-2/3 text-left font-bold">NCR - Manila</span>
+                  <span className="w-2/3 text-left font-bold">{personalDetails.birthPlace}</span>
                 </div>
               </div>
               <div className="w-full flex flex-row flex-start">
                 <div className="w-1/3 flex flex-row flex-start">
                   <span className="w-1/3 text-left">Gender :</span>
-                  <span className="w-2/3 text-left font-bold">Male</span>
+                  <span className="w-2/3 text-left font-bold">{personalDetails.gender}</span>
                 </div>
                 <div className="w-1/3 flex flex-row flex-start">
                   <span className="w-1/3 text-left">Civil Status :</span>
-                  <span className="w-2/3 text-left font-bold">Status</span>
+                  <span className="w-2/3 text-left font-bold">{personalDetails.civilStatus}</span>
                 </div>
                 <div className="w-1/3 flex flex-row flex-start">
                   <span className="w-1/3 text-left">Citizenship :</span>
-                  <span className="w-2/3 text-left font-bold">Filipino</span>
+                  <span className="w-2/3 text-left font-bold">{personalDetails.citizenship}</span>
                 </div>
               </div>
               <div className="w-full flex flex-row flex-start">
                 <div className="w-1/3 flex flex-row flex-start">
                   <span className="w-1/3 text-left">Mobile No. :</span>
-                  <span className="w-2/3 text-left font-bold">0998 765 4321</span>
+                  <span className="w-2/3 text-left font-bold">{personalDetails.mobileNumber}</span>
                 </div>
                 <div className="w-1/3 flex flex-row flex-start">
                   <span className="w-1/3 text-left">Email Address :</span>
-                  <span className="w-2/3 text-left font-bold">juan.delacruz@gmail.com</span>
+                  <span className="w-2/3 text-left font-bold">{personalDetails.emailAddress}</span>
                 </div>
               </div>
             </div>
@@ -98,15 +127,15 @@ const FacultyProfile = () => {
               <div className="w-full flex flex-row flex-start">
                 <div className="w-1/3 flex flex-row flex-start">
                   <span className="w-1/3 text-left">TIN :</span>
-                  <span className="w-2/3 text-left font-bold">xxx-xxx-xxx-xxx</span>
+                  <span className="w-2/3 text-left font-bold">{employmentDetails.tin}</span>
                 </div>
                 <div className="w-1/3 flex flex-row flex-start">
                   <span className="w-1/3 text-left">GSIS No. :</span>
-                  <span className="w-2/3 text-left font-bold">xxxx-xxxx-xxxx-xxxx</span>
+                  <span className="w-2/3 text-left font-bold">{employmentDetails.gsisNumber}</span>
                 </div>
                 <div className="w-1/3 flex flex-row flex-start">
                   <span className="w-1/3 text-left">Instructor Code :</span>
-                  <span className="w-2/3 text-left font-bold">DELACRUZJP</span>
+                  <span className="w-2/3 text-left font-bold">{employmentDetails.instructorCode}</span>
                 </div>
               </div>
             </div>
@@ -120,13 +149,13 @@ const FacultyProfile = () => {
               <div className="w-full flex flex-row flex-start">
                 <div className="w-1/2 flex flex-row flex-start">
                   <span className="w-1/4 text-left">Address :</span>
-                  <span className="w-full text-left font-bold">5317 Muralla St., Intramuros, Manila, Philippines 1002</span>
+                  <span className="w-full text-left font-bold">{currentAddress.address}</span>
                 </div>
               </div>
               <div className="w-full flex flex-row flex-start">
                 <div className="w-1/2 flex flex-row flex-start">
                   <span className="w-1/4 text-left">Phone No. :</span>
-                  <span className="w-full text-left font-bold">(02) 8643 2500</span>
+                  <span className="w-full text-left font-bold">{currentAddress.phoneNumber}</span>
                 </div>
               </div>
             </div>
