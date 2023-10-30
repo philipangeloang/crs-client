f40dc212a43b500f655e7d97c5baa886b0e6a833
Roi John Belmonte <roibelmonte@gmail.com>
2023-10-30 17:56:07 +0800

    Added Edit Details for Faculty Profile

--- src/pages/Faculty/FacultyProfile.tsx
+++ src/pages/Faculty/FacultyProfile.tsx
@@ -3,6 +3,7 @@ import { FiFacebook, FiTwitter } from "react-icons/fi";
 import { BsGlobe2 } from "react-icons/bs";
 import { BiUserPin } from "react-icons/bi";
 import DateTime from "@/components/DateTime";
+import { Link } from "react-router-dom";
 
 // Faculty Details
 let personalDetails = {
@@ -13,7 +14,7 @@ let personalDetails = {
   nameExtension: "",
   maidenName: "",
   pedigree: "None",
-  birthDate: "May 3, 1987",
+  birthDate: "1987-05-03",
   birthPlace: "NCR - Manila",
   gender: "Male",
   civilStatus: "Single",
@@ -32,6 +33,9 @@ let currentAddress = {
   address: "5317 Muralla St., Intramuros, Manila, Philippines 1002",
   phoneNumber: "(02) 8643 2500",
 };
+
+let stringBirthDate = new Date(personalDetails.birthDate);
+const displayBirthDate = new Intl.DateTimeFormat('en-PH', {year: 'numeric', month: 'long', day: 'numeric'}).format(stringBirthDate);
 
 const FacultyProfile = () => {
   return (
@@ -52,66 +56,70 @@ const FacultyProfile = () => {
 
         {/* Row 2 */}
         <div className="col-span-12 mt-12">
-          <div className="col-span-12 gap-5 flex flex-start items-center">
+          <div className="col-span-12 gap-5 flex justify-start items-center">
             <div className="px-4 py-2 bg-main-blue text-white rounded-lg ">
               View Details
             </div>
             <div className="px-4 py-2 bg-main-red text-white rounded-lg cursor-pointer">
-              Edit Details
+              <Link
+                to="/home/profile/edit"
+              >
+                Edit Details
+              </Link>
             </div>
           </div>
 
           <h1 className="font-bold text-[#434343] text-1xl mt-4">
             Personal Details
           </h1>
-          <div className="col-span-12 flex flex-start text-sm items-center">
+          <div className="col-span-12 flex justify-start text-sm items-center">
             <div className="p-4 mt-4 gap-4 rounded-lg border border-black w-full flex flex-col justify-center">
-              <div className="w-full flex flex-row flex-start">
-                <div className="w-1/3 flex flex-row flex-start">
+              <div className="w-full flex flex-row justify-start">
+                <div className="w-1/3 flex flex-row justify-start items-center">
                   <span className="w-1/3 text-left">Employee No. :</span>
                   <span className="w-2/3 text-left font-bold">{personalDetails.employeeNumber}</span>
                 </div>
               </div>
-              <div className="w-full flex flex-row flex-start">
-                <div className="w-1/3 flex flex-row flex-start">
+              <div className="w-full flex flex-row justify-start">
+                <div className="w-1/3 flex flex-row justify-start items-center">
                   <span className="w-1/3 text-left">Full Name :</span>
                   <span className="w-2/3 text-left font-bold">{personalDetails.firstName} {personalDetails.middleName} {personalDetails.lastName} {personalDetails.nameExtension}</span>
                 </div>
-                <div className="w-1/3 flex flex-row flex-start">
+                <div className="w-1/3 flex flex-row justify-start items-center">
                   <span className="w-1/3 text-left">Pedigree :</span>
                   <span className="w-2/3 text-left font-bold">{personalDetails.pedigree}</span>
                 </div>
               </div>
-              <div className="w-full flex flex-row flex-start">
-                <div className="w-1/3 flex flex-row flex-start">
+              <div className="w-full flex flex-row justify-start">
+                <div className="w-1/3 flex flex-row justify-start items-center">
                   <span className="w-1/3 text-left">Birth Date :</span>
-                  <span className="w-2/3 text-left font-bold">{personalDetails.birthDate}</span>
+                  <span className="w-2/3 text-left font-bold">{displayBirthDate}</span>
                 </div>
-                <div className="w-1/3 flex flex-row flex-start">
+                <div className="w-1/3 flex flex-row justify-start items-center">
                   <span className="w-1/3 text-left">Birth Place :</span>
                   <span className="w-2/3 text-left font-bold">{personalDetails.birthPlace}</span>
                 </div>
               </div>
-              <div className="w-full flex flex-row flex-start">
-                <div className="w-1/3 flex flex-row flex-start">
+              <div className="w-full flex flex-row justify-start">
+                <div className="w-1/3 flex flex-row justify-start items-center">
                   <span className="w-1/3 text-left">Gender :</span>
                   <span className="w-2/3 text-left font-bold">{personalDetails.gender}</span>
                 </div>
-                <div className="w-1/3 flex flex-row flex-start">
+                <div className="w-1/3 flex flex-row justify-start items-center">
                   <span className="w-1/3 text-left">Civil Status :</span>
                   <span className="w-2/3 text-left font-bold">{personalDetails.civilStatus}</span>
                 </div>
-                <div className="w-1/3 flex flex-row flex-start">
+                <div className="w-1/3 flex flex-row justify-start items-center">
                   <span className="w-1/3 text-left">Citizenship :</span>
                   <span className="w-2/3 text-left font-bold">{personalDetails.citizenship}</span>
                 </div>
               </div>
-              <div className="w-full flex flex-row flex-start">
-                <div className="w-1/3 flex flex-row flex-start">
+              <div className="w-full flex flex-row justify-start">
+                <div className="w-1/3 flex flex-row justify-start items-center">
                   <span className="w-1/3 text-left">Mobile No. :</span>
                   <span className="w-2/3 text-left font-bold">{personalDetails.mobileNumber}</span>
                 </div>
-                <div className="w-1/3 flex flex-row flex-start">
+                <div className="w-1/3 flex flex-row justify-start items-center">
                   <span className="w-1/3 text-left">Email Address :</span>
                   <span className="w-2/3 text-left font-bold">{personalDetails.emailAddress}</span>
                 </div>
@@ -122,18 +130,18 @@ const FacultyProfile = () => {
           <h1 className="font-bold text-[#434343] text-1xl mt-4">
             Employment Details
           </h1>
-          <div className="col-span-12 flex flex-start text-sm items-center">
+          <div className="col-span-12 flex justify-start text-sm items-center">
             <div className="p-4 mt-4 gap-4 rounded-lg border border-black w-full flex flex-col justify-center">
-              <div className="w-full flex flex-row flex-start">
-                <div className="w-1/3 flex flex-row flex-start">
+              <div className="w-full flex flex-row justify-start">
+                <div className="w-1/3 flex flex-row justify-start items-center">
                   <span className="w-1/3 text-left">TIN :</span>
                   <span className="w-2/3 text-left font-bold">{employmentDetails.tin}</span>
                 </div>
-                <div className="w-1/3 flex flex-row flex-start">
+                <div className="w-1/3 flex flex-row justify-start items-center">
                   <span className="w-1/3 text-left">GSIS No. :</span>
                   <span className="w-2/3 text-left font-bold">{employmentDetails.gsisNumber}</span>
                 </div>
-                <div className="w-1/3 flex flex-row flex-start">
+                <div className="w-1/3 flex flex-row justify-start items-center">
                   <span className="w-1/3 text-left">Instructor Code :</span>
                   <span className="w-2/3 text-left font-bold">{employmentDetails.instructorCode}</span>
                 </div>
@@ -144,16 +152,16 @@ const FacultyProfile = () => {
           <h1 className="font-bold text-[#434343] text-1xl mt-4">
             Current Address
           </h1>
-          <div className="col-span-12 flex flex-start text-sm items-center">
+          <div className="col-span-12 flex justify-start text-sm items-center">
             <div className="p-4 mt-4 gap-4 rounded-lg border border-black w-full flex flex-col justify-center">
-              <div className="w-full flex flex-row flex-start">
-                <div className="w-1/2 flex flex-row flex-start">
+              <div className="w-full flex flex-row justify-start">
+                <div className="w-1/2 flex flex-row justify-start items-center">
                   <span className="w-1/4 text-left">Address :</span>
                   <span className="w-full text-left font-bold">{currentAddress.address}</span>
                 </div>
               </div>
-              <div className="w-full flex flex-row flex-start">
-                <div className="w-1/2 flex flex-row flex-start">
+              <div className="w-full flex flex-row justify-start">
+                <div className="w-1/2 flex flex-row justify-start items-center">
                   <span className="w-1/4 text-left">Phone No. :</span>
                   <span className="w-full text-left font-bold">{currentAddress.phoneNumber}</span>
                 </div>
