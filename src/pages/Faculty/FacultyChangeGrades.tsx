bde11cbc28305a8fed3d59178f433bc89ea95d70
Roi John Belmonte <roibelmonte@gmail.com>
2023-11-08 00:41:35 +0800

    Added modal component
    Temporary modal currently in-place in this commit for Change Grades module

--- src/pages/Faculty/FacultyChangeGrades.tsx
+++ src/pages/Faculty/FacultyChangeGrades.tsx
@@ -1,9 +1,10 @@
 import { AiOutlineWarning } from "react-icons/ai";
 import { FaEdit, FaArrowRight } from 'react-icons/fa';
 import DateTime from "@/components/DateTime";
-import { Key } from "react";
+import { Key, useState } from "react";
 import StringCardinal from "@/components/StringCardinal";
 import { Link } from "react-router-dom";
+import { FullModal } from "@/components/ui/modal";
 
 // Faculty Details
 type termAndAcademicYear = {
@@ -89,44 +90,18 @@ function SelectTermsAndAcademicYears({ data }: termAndAcademicYearArray) {
   );
 }
 
-function TableChangeGrade ({ data }: studentArray) {
-  return (
-    <table id="tableChangeGrade" className="w-full table-auto rounded-md my-4 py-2 px-4 border text-left">
-      <thead>
-        <tr className="bg-main-red text-white font-bold">
-          <th className="p-4">Subject Title</th>
-          <th>Course Code & Section</th>
-          <th>Student No.</th>
-          <th>Student Name</th>
-          <th>College</th>
-          <th>Year</th>
-          <th>Grade</th>
-          <th className="text-center">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((student: student) => (
-          <tr className="border-b-2 border-black text-sm hover:bg-neutral-100">
-            <td className="p-4">CS Elective 2 (Lec)</td>
-            <td>CSC 0413</td>
-            <td>{student.student_number}</td>
-            <td>{student.name}</td>
-            <td>{student.college}</td>
-            <td>{student.year}</td>
-            <td>{student.grade}</td>
-            <td className="align-middle">
-              <div className="text-center gap-2 flex justify-center items-center">
-                <FaEdit className="bg-main-blue text-white rounded w-8 h-8 p-2 cursor-pointer" />
-              </div>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
-
 const FacultyChangeGrades = () => {
+  const [ showGradeChangeModal, setGradeChangeModal ] = useState(false);
+
+  const handleGradeClose = () => {
+    setGradeChangeModal(false);
+  }
+
+  const handleGradeProceed = () => {
+    alert("Successfully changed grade!");
+    setGradeChangeModal(false);
+  }
+
   return (
     <>
       <div className="p-10 px-16 grid grid-cols-12 font-montserrat ">
@@ -172,18 +147,55 @@ const FacultyChangeGrades = () => {
               </div>
             </div>
           </div>
-          
+
           <div className="col-span-12 gap-5 flex justify-start items-center">
-            <TableChangeGrade data={students}/>
+            { /* This contains the table for changing the grades */ }
+            <table id="tableChangeGrade" className="w-full table-auto rounded-md my-4 py-2 px-4 border text-left">
+              <thead>
+                <tr className="bg-main-red text-white font-bold">
+                  <th className="p-4">Subject Title</th>
+                  <th>Course Code & Section</th>
+                  <th>Student No.</th>
+                  <th>Student Name</th>
+                  <th>College</th>
+                  <th>Year</th>
+                  <th>Grade</th>
+                  <th className="text-center">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {students.map((student: student) => (
+                  <tr className="border-b-2 border-black text-sm hover:bg-neutral-100">
+                    <td className="p-4">CS Elective 2 (Lec)</td>
+                    <td>CSC 0413</td>
+                    <td>{student.student_number}</td>
+                    <td>{student.name}</td>
+                    <td>{student.college}</td>
+                    <td>{student.year}</td>
+                    <td>{student.grade}</td>
+                    <td className="align-middle">
+                      <div className="text-center gap-2 flex justify-center items-center" onClick={() => setGradeChangeModal(true)}>
+                        <FaEdit className="bg-main-blue text-white rounded w-8 h-8 p-2 cursor-pointer"/>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
+
+        { showGradeChangeModal && (
+            <FullModal modalHeading="Title" onClose={handleGradeClose} onProceed={handleGradeProceed}>
+              <p>This is the modal content.</p>
+            </FullModal>
+          )}
 
         {/* Row 3 */}
         <div className="col-span-12 flex justify-between items-end mt-24 text-[#434343] text-sm">
           <div className="flex gap-10">
           </div>
-
-          <DateTime />
+          <DateTime/>
         </div>
       </div>
     </>
